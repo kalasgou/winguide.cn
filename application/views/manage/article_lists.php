40165927b290bf9527ebf0dda95a4f0fce6a6ebf
kalasgou <duduleopard@gmail.com>
2015-04-27 19:04:43 +0800

    test

--- application/views/manage/article_lists.php
+++ application/views/manage/article_lists.php
@@ -83,58 +83,52 @@
 			</div>
 		</div>
 	</div>
+
 	<div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
-    <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-        <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
-      </div>
-          <div class="modal-body">
-          <div class="form-group">
-        <input class="form-control " type="text" placeholder="Mohsin">
-        </div>
-        <div class="form-group">
-        
-        <input class="form-control " type="text" placeholder="Irshad">
-        </div>
-        <div class="form-group">
-        <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
-    
-        
-        </div>
-      </div>
-          <div class="modal-footer ">
-        <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-      </div>
-        </div>
-    <!-- /.modal-content --> 
-  </div>
-      <!-- /.modal-dialog --> 
-    </div>
-    
-    
-    
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+					<h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<input class="form-control " type="text" placeholder="Mohsin">
+					</div>
+					<div class="form-group">
+						<input class="form-control " type="text" placeholder="Irshad">
+					</div>
+					<div class="form-group">
+						<textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
+					</div>
+				</div>
+				<div class="modal-footer ">
+					<button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+				</div>
+			</div>
+		<!-- /.modal-content --> 
+		</div>
+		<!-- /.modal-dialog --> 
+	</div>
+	
     <div class="modal fade" id="delete" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
-      <div class="modal-dialog">
-    <div class="modal-content">
-          <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-        <h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
-      </div>
-          <div class="modal-body">
-       
-       <div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this Record?</div>
-       
-      </div>
-        <div class="modal-footer ">
-        <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
-        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
-      </div>
-        </div>
-    <!-- /.modal-content --> 
-  </div>
-      <!-- /.modal-dialog --> 
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+					<h4 class="modal-title custom_align" id="Heading">Delete this entry</h4>
+				</div>
+				<div class="modal-body">
+					<div class="alert alert-danger"><span class="glyphicon glyphicon-warning-sign"></span> Are you sure you want to delete this Record?</div>
+				</div>
+				<div class="modal-footer ">
+					<button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
+				</div>
+			</div>
+		<!-- /.modal-content --> 
+		</div>
+		<!-- /.modal-dialog --> 
     </div>
 </div>
 </div>
@@ -166,6 +160,58 @@
 		$('.module-options').change(function() {
 			var _module_id = $(this).find('option:selected').data('module-id'); 
 			$('#module-id').val(_module_id);
+		});
+		
+		$('td a').click(function() {
+			var _entry_id = $($(this).parent().siblings('td')[0]).html()
+			$('input.hidden-id').val(_entry_id);
+			
+			var _cur_title = $($(this).parent().siblings('td')[2]).html();
+			
+			switch ($(this).data('title')) {
+				case 'Edit':
+							$('#edit input.title').val(_cur_title);
+							break;
+				case 'Delete':
+							$('#delete b.hints').html(_cur_title);
+							break;
+			}
+		});
+		
+		$('#edit button.update').click(function() {
+			var _entry_id = $('input.hidden-id').val();
+			var _title = $('#edit input.title').val()
+			
+			$.ajax({
+				url: '<?= base_url('')?>',
+				data: {entry_id:_entry_id, title:_title},
+				type: 'post',
+				dataType: 'json',
+				success: function(json) {
+					location.reload();
+				},
+				error: function() {
+					alert('Network Error');
+					location.reload();
+				}
+			});
+		});
+		
+		$('#delete button.confirm').click(function() {
+			var _entry_id = $('input.hidden-id').val();
+			
+			$.ajax({
+				url: '<?= base_url('')?>',
+				data: {entry_id:_entry_id},
+				type: 'post',
+				dataType: 'json',
+				success: function(json) {
+					location.reload();
+				},
+				error: function() {
+					alert('Network Error');
+				}
+			});
 		});
     });
 </script>
--- application/views/manage/navbar.php
+++ application/views/manage/navbar.php
@@ -368,107 +368,107 @@ $(function () {
   });
 </script>
 <div class="wrap">
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle navbar-toggle-sidebar collapsed">菜单</button>
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="http://www.winguide.cn">
-				WinGuide赢凯
-			</a>
-		</div>
-
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
-			<!--<form class="navbar-form navbar-left" method="GET" role="search">
-				<div class="form-group">
-					<input type="text" name="q" class="form-control" placeholder="Search">
-				</div>
-				<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-			</form>-->
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="http://www.winguide.cn" target="_blank">
-					<span class="glyphicon glyphicon-home"></span> 主站首页
-					</a></li>
-				<li class="dropdown ">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-					<span class="glyphicon glyphicon-user"></span> 当前帐号
-					<span class="caret"></span></a>
-					<ul class="dropdown-menu" role="menu">
-						<!--<li class="dropdown-header">设置</li>-->
-						<!--<li class=""><a href="#"><span class="glyphicon glyphicon-cog"></span> Other Link</a></li>
-						<li class=""><a href="#"><span class="glyphicon glyphicon-cog"></span> Other Link</a></li>
-						<li class=""><a href="#"><span class="glyphicon glyphicon-cog"></span> Other Link</a></li>
-						<li class="divider"></li>-->
-						<li><a href="#"><span class="glyphicon glyphicon-off"></span> 退出系统</a></li>
-					</ul>
-				</li>
-			</ul>
-		</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
-</nav>
-
-<div class="container-fluid main-container">
-  	<div class="col-md-2 sidebar">
-  		<div class="row">
-			<!-- uncomment code for absolute positioning tweek see top comment in css -->
-			<div class="absolute-wrapper"> </div>
-			<!-- Menu -->
-			<div class="side-menu">
-				<nav class="navbar navbar-default" role="navigation">
-					<!-- Main Menu -->
-					<div class="side-menu-container">
-						<input id="hover" type="hidden" value="<?= $hover ?>" />
-						<ul class="nav navbar-nav">
-							<li id="article_nav"><a href="/console/article"><span class="glyphicon glyphicon-globe"></span> 文章仓库</a></li>
-							<li id="student_nav"><a href="/console/student"><span class="glyphicon glyphicon-education"></span> 付费帐号</a></li>
-							<li id="user_nav"><a href="/console/user"><span class="glyphicon glyphicon-sunglasses"></span> 普通帐号</a></li>
-
-							<!-- Dropdown-->
-							<li class="panel panel-default" id="dropdown">
-								<a data-toggle="collapse" href="#dropdown-lvl1">
-									<span class="glyphicon glyphicon-edit"></span> 讨论留言 <span class="caret"></span>
-								</a>
-
-								<!-- Dropdown level 1 -->
-								<div id="dropdown-lvl1" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul class="nav navbar-nav">
-											<li id="forum_public_nav"><a href="/console/forum?visibility=public"><span class="glyphicon glyphicon-bullhorn"></span> 全站论坛</a></li>
-											<li id="forum_course_nav"><a href="/console/forum?visibility=course"><span class="glyphicon glyphicon-book"></span> 课程任务</a></li>
-											<!--<li><a href="#">Link</a></li>
-
-											 Dropdown level 2 
-											<li class="panel panel-default" id="dropdown">
-												<a data-toggle="collapse" href="#dropdown-lvl2">
-													<span class="glyphicon glyphicon-off"></span> Sub Level <span class="caret"></span>
-												</a>
-												<div id="dropdown-lvl2" class="panel-collapse collapse">
-													<div class="panel-body">
-														<ul class="nav navbar-nav">
-															<li><a href="#">Link</a></li>
-															<li><a href="#">Link</a></li>
-															<li><a href="#">Link</a></li>
-														</ul>
-													</div>
-												</div>
-											</li>-->
-										</ul>
-									</div>
-								</div>
-							</li>
-							
-							<li id="admin_nav"><a href="/console/admin"><span class="glyphicon glyphicon-user"></span> 后台帐号</a></li>
-							<li><a href="#"><span class="glyphicon glyphicon-hdd"></span> 日志记录</a></li>
-						</ul>
-					</div><!-- /.navbar-collapse -->
-				</nav>
+	<nav class="navbar navbar-default navbar-static-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle navbar-toggle-sidebar collapsed">菜单</button>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="http://www.winguide.cn">
+					WinGuide赢凯
+				</a>
 			</div>
-		</div>  		
-	</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
+				<!--<form class="navbar-form navbar-left" method="GET" role="search">
+					<div class="form-group">
+						<input type="text" name="q" class="form-control" placeholder="Search">
+					</div>
+					<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+				</form>-->
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="http://www.winguide.cn" target="_blank">
+						<span class="glyphicon glyphicon-home"></span> 主站首页
+						</a></li>
+					<li class="dropdown ">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+						<span class="glyphicon glyphicon-user"></span> 当前帐号
+						<span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<!--<li class="dropdown-header">设置</li>-->
+							<!--<li class=""><a href="#"><span class="glyphicon glyphicon-cog"></span> Other Link</a></li>
+							<li class=""><a href="#"><span class="glyphicon glyphicon-cog"></span> Other Link</a></li>
+							<li class=""><a href="#"><span class="glyphicon glyphicon-cog"></span> Other Link</a></li>
+							<li class="divider"></li>-->
+							<li><a href="#"><span class="glyphicon glyphicon-off"></span> 退出系统</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+
+	<div class="container-fluid main-container">
+		<div class="col-md-2 sidebar">
+			<div class="row">
+				<!-- uncomment code for absolute positioning tweek see top comment in css -->
+				<div class="absolute-wrapper"> </div>
+				<!-- Menu -->
+				<div class="side-menu">
+					<nav class="navbar navbar-default" role="navigation">
+						<!-- Main Menu -->
+						<div class="side-menu-container">
+							<input id="hover" type="hidden" value="<?= $hover ?>" />
+							<ul class="nav navbar-nav">
+								<li id="article_nav"><a href="/console/article"><span class="glyphicon glyphicon-globe"></span> 文章仓库</a></li>
+								<li id="student_nav"><a href="/console/student"><span class="glyphicon glyphicon-education"></span> 付费帐号</a></li>
+								<li id="user_nav"><a href="/console/user"><span class="glyphicon glyphicon-sunglasses"></span> 普通帐号</a></li>
+
+								<!-- Dropdown-->
+								<li class="panel panel-default" id="dropdown">
+									<a data-toggle="collapse" href="#dropdown-lvl1">
+										<span class="glyphicon glyphicon-edit"></span> 讨论留言 <span class="caret"></span>
+									</a>
+
+									<!-- Dropdown level 1 -->
+									<div id="dropdown-lvl1" class="panel-collapse collapse">
+										<div class="panel-body">
+											<ul class="nav navbar-nav">
+												<li id="forum_public_nav"><a href="/console/forum?visibility=public"><span class="glyphicon glyphicon-bullhorn"></span> 全站论坛</a></li>
+												<li id="forum_course_nav"><a href="/console/forum?visibility=course"><span class="glyphicon glyphicon-book"></span> 课程任务</a></li>
+												<!--<li><a href="#">Link</a></li>
+
+												 Dropdown level 2 
+												<li class="panel panel-default" id="dropdown">
+													<a data-toggle="collapse" href="#dropdown-lvl2">
+														<span class="glyphicon glyphicon-off"></span> Sub Level <span class="caret"></span>
+													</a>
+													<div id="dropdown-lvl2" class="panel-collapse collapse">
+														<div class="panel-body">
+															<ul class="nav navbar-nav">
+																<li><a href="#">Link</a></li>
+																<li><a href="#">Link</a></li>
+																<li><a href="#">Link</a></li>
+															</ul>
+														</div>
+													</div>
+												</li>-->
+											</ul>
+										</div>
+									</div>
+								</li>
+								
+								<li id="admin_nav"><a href="/console/admin"><span class="glyphicon glyphicon-user"></span> 后台帐号</a></li>
+								<li><a href="#"><span class="glyphicon glyphicon-hdd"></span> 日志记录</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</nav>
+				</div>
+			</div>  		
+		</div>
